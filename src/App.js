d6de5f33e57311e804242cfada0251086924ea07
Vivekkumarv123 <164018649+Vivekkumarv123@users.noreply.github.com>
2024-08-09 16:06:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Pages/Home';
-import Login from './Components/Pages/Login';
+import SDGs from './Components/Pages/SDGs';
 import Signup from './Components/Pages/Signup';
 import About from './Components/Pages/About';
 
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About/>}/>
-        <Route path="/Login" element={<Login />} />
+        <Route path="/SDGs" element={<SDGs />} />
         <Route path="/Signup" element={<Signup />} />
       </Routes>
     </Router>
